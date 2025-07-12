6955fc0442a12def8089802dcc3be921a07b617d
343 <774657338@qq.com>
2025-07-12 12:54:29 +0800

    init

---
+++ src/main/java/com/bookshare/book_platform/BookPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.bookshare.book_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookshare/book_platform/BookPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookshare.book_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
